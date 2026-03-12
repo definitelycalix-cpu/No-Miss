4522ea427e8e539e6f55e8e830b7f67fdfab6a2e
definitelycalix-cpu <definitelycalix@gmail.com>
2026-03-12 14:51:09 +0800

    Update NoMissClient.java

--- src/main/java/me/seyit/NoMissClient.java
+++ src/main/java/me/seyit/NoMissClient.java
@@ -5,7 +5,6 @@ import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
 import net.minecraft.client.option.KeyBinding;
 import net.minecraft.client.util.InputUtil;
-import net.minecraft.text.Text;
 import org.lwjgl.glfw.GLFW;
 
 public class NoMissClient implements ClientModInitializer {
@@ -24,12 +23,6 @@ public class NoMissClient implements ClientModInitializer {
         ClientTickEvents.END_CLIENT_TICK.register(client -> {
             while (toggleKey.wasPressed()) {
                 enabled = !enabled;
-                if (client.player != null) {
-                    client.player.sendMessage(
-                            Text.literal(enabled ? "§aNo Miss ON" : "§cNo Miss OFF"),
-                            true
-                    );
-                }
             }
         });
     }
