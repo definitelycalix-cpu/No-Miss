75cd7194fead3585de01d7eec6215dbefbe0c4b5
definitelycalix-cpu <definitelycalix@gmail.com>
2026-03-12 15:21:47 +0800

    Update NoMissClient.java

--- src/main/java/me/seyit/NoMissClient.java
+++ src/main/java/me/seyit/NoMissClient.java
@@ -3,27 +3,25 @@ package me.seyit;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
-import net.minecraft.client.KeyMapping;
+import net.minecraft.client.option.KeyBinding;
 import net.minecraft.client.util.InputUtil;
 import org.lwjgl.glfw.GLFW;
 
 public class NoMissClient implements ClientModInitializer {
-
     private static boolean enabled = true;
-    private static KeyMapping toggleKey;
+    private static KeyBinding toggleKey;
 
     @Override
     public void onInitializeClient() {
-
-        toggleKey = KeyBindingHelper.registerKeyBinding(new KeyMapping(
-                "key.no_miss.toggle",
+        toggleKey = KeyBindingHelper.registerKeyBinding(new KeyBinding(
+                "key.no-miss.toggle",
                 InputUtil.Type.KEYSYM,
                 GLFW.GLFW_KEY_N,
-                "key.categories.no_miss"
+                "category.no-miss"
         ));
 
         ClientTickEvents.END_CLIENT_TICK.register(client -> {
-            while (toggleKey.consumeClick()) {
+            while (toggleKey.wasPressed()) {
                 enabled = !enabled;
             }
         });
